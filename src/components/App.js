1f1440fc58e03043186fb3d0d7180da9a846ad5c
Sean <ninetailsdevelopment@gmail.com>
2020-05-05 08:18:34 +0100

    api play

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import Homage from "./HomePage";
+import HomPage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
+import CoursesPage from "./CoursesPage";
 
 function App() {
   function getPage() {
     const route = window.location.pathname;
-    if (route == "/about") return <AboutPage />;
-    return <Homage />;
+    if (route === "/courses") return <CoursesPage />;
+    if (route === "/about") return <AboutPage />;
+    return <HomPage />;
   }
 
   return (
